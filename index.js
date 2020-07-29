8b9fb8fbf5be504e5fd7d0b24322a626d4dfcc76
hanoelleb <hanoelleb@gmail.com>
2020-07-28 22:49:16 -0700

    can start, stop, and reset timer

--- index.js
+++ index.js
@@ -2,7 +2,25 @@ const WORK_TIME = 1500; //25 minutes
 const SHORT_TIME = 300; //5 minutes
 const LONG_TIME = 600; //10 minutes
 
+var CURRENT_TIME = WORK_TIME;
+var CHOSEN_TIME = CURRENT_TIME;
+
+var RUNNING = true;
+
+var handler = function() {
+  var currMins = parseInt(CURRENT_TIME / 60);
+  var currSecs = parseInt(CURRENT_TIME % 60);
+  document.getElementById('timer').textContent = 'Time: ' + 
+      (currMins < 10 ? "0" + currMins : currMins) + ":" 
+          + (currSecs < 10 ? "0" + currSecs : currSecs);
+  CURRENT_TIME--;
+};
+
+//handler();
+
 $(document).ready( function() {
+    var timer = setInterval(handler, 1000);
+
     $('#explain').hide();
 
     $('#about').click(function() {
@@ -18,6 +36,7 @@ $(document).ready( function() {
     $('#work').click( function() {
         $('#timer').text('Time: 25:00');
         $('#Pomo').attr('src', 'tomato-work.png');
+
     });
 
     $('#short').click( function() {
@@ -29,4 +48,25 @@ $(document).ready( function() {
         $('#timer').text('Time: 10:00');
         $('#Pomo').attr('src', 'tomato-long.png')
     });
+
+   $('#start').click( function() {
+       if (!RUNNING) {
+           timer = setInterval(handler, 1000);
+           RUNNING = true;
+       }
+   });
+
+   $('#stop').click( function() {
+       RUNNING = false;
+       clearInterval(timer);
+   });
+
+   $('#reset').click( function() {
+       CURRENT_TIME = CHOSEN_TIME;
+       clearInterval(timer);
+       if (!RUNNING)
+           handler();
+       else
+	   timer = setInterval(handler, 1000);
+   });
 });
